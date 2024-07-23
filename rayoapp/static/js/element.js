010475ee7be0c5d14a622505aa288610ac0777fb
roydev-dw <ado.pezzini@gmail.com>
2024-07-22 22:13:52 -0400

    se eliminan archivos innecesarios

--- rayoapp/static/js/element.js
+++
@@ -1,9 +0,0 @@
-//SCROLL BOTON BACK TO END
-
-document.getElementById("button-up").addEventListener("click", scrollUp);
-function scrollUp() {
-    var currentScroll = document.documentElement.scrollTop;
-    if (currentScroll > 0) {
-        window.scrollTo(0, 0);
-    }
-}
